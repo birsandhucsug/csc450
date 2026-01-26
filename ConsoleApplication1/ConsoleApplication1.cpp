b592ec65b365ebea59b428df845da4b88a6179ca
birs0x0 <bir.s0x0@gmail.com>
2026-01-26 03:54:56 +0100

    Create ConsoleApplication1.cpp

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "First Name: John" << endl;
+    cout << "Last Name: Doe" << endl;
+    cout << "Street Address: 123 Main Street" << endl;
+    cout << "City: Springfield" << endl;
+    cout << "Zip Code: 12345" << endl;
+    return 0;
+}
